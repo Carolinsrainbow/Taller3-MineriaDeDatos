da251c0cfc9b0c5172321f181b35a1d193f03c62
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-04 18:19:36 -0400

    Add files via upload

--- Taller 3 - Carolina Herrera Azolas.R
+++ Taller 3 - Carolina Herrera Azolas.R
@@ -1,5 +1,69 @@
 ### Taller 3 
-
+## Ingreso de datos
 datos <- read.csv("/Users/user/Desktop/lastfm.csv",sep=";", header=TRUE)
 
+## Incorporación de paquetes
+install.packages("arules")
+install.packages("arulesViz")
 
+library(arules)
+library(arulesViz)
+
+## Análisis base 
+
+## Resumen estadístico
+summary(datos)
+
+
+## Cantidad de bandas 
+length(unique(datos$artist))
+
+##Cantidad de tipos de usuarios
+length(unique(datos$user))
+
+##cantidad de sub categorias de artistas
+length(unique(datos$size))
+
+##cantidad de sub categorias de artistas
+length(unique(datos$artist_id))
+
+
+# Generamos la lista de transacciones a utilizar
+write.table(datos, file = tmp <- file(), row.names = FALSE)
+listas <- read.transactions(tmp,
+                            format = "single",
+                            header = TRUE,
+                            cols = c("user", "artist"))
+close(tmp)
+summary(listas)
+
+# Recomendacion: no utilizar el comando image(listas) pues son demasiadas
+
+resultados2 <- apriori(listas,
+                       parameter = list(supp = 0.01,
+                                        conf = 0.5,
+                                        minlen = 2,
+                                        maxlen = 10,
+                                        maxtime = 5))
+
+
+# Analizamos los resultados
+summary(resultados2)
+inspect(resultados2)
+head(resultados2)
+# Algunas representaciones graficas
+plot(resultados2, method = "paracoord")
+plot(resultados2, method = "graph")
+
+
+# Analisis ordenado por criterios
+reglas_conf <- sort(resultados2,
+                    by = "confidence",
+                    decreasing = TRUE)
+inspect(head(reglas_conf))
+
+
+reglas_lift <- sort (resultados2,
+                     by = "lift",
+                     decreasing=TRUE)
+inspect(head(reglas_lift))
